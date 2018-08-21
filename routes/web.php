9afc703722bb9feee7f838f12fed82927d9b1943
mauriciozatt@hotmail.com <mauriciozatt@hotmail.com>
2018-08-21 20:48:29 -0300

    rota  > controller > view

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Esta Rota chama o metodo  CadastroCliente do controllerr (Cadastro_Controller)
+Route::get('/cliente',['uses'=>'Cadastro_Controller@CadastroCliente']);
